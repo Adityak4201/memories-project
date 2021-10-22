1dad846b26107d913b12a40f98e81218bf010e7a
Aditya Khandelwal <adityakhandelwal4201@gmail.com>
2021-10-22 16:26:14 +0000

    Node express mongoose setup

---
+++ server/src/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectMongoDB from "./db/connect.js";
+
+const main = async () => {
+  const app = express();
+  dotenv.config();
+  const port = process.env.PORT || 4000;
+  connectMongoDB();
+  app.use(express.json({ limit: "30mb", extended: true }));
+  app.use(express.urlencoded({ limit: "30mb", extended: true }));
+  app.use(cors());
+
+  app.get("/", function (req, res) {
+    res.send("Server is up and running");
+  });
+
+  app.listen(port, () => {
+    console.log("Server Running at port", port);
+  });
+};
+
+main();
